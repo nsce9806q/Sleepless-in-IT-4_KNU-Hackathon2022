efbf8448bf16e70d10caa9ea136ee4e8ab6c0f87
ChangEn Yea <nsce9806q@gmail.com>
2022-09-21 17:20:19 +0900

    First Commit

---
+++ backend/src/main/java/sleeplessinit4/hanzip/HanzipApplication.java
@@ -0,0 +1,13 @@
+package sleeplessinit4.hanzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanzipApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/sleeplessinit4/hanzip/HanzipApplicationTests.java
@@ -0,0 +1,13 @@
+package sleeplessinit4.hanzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
