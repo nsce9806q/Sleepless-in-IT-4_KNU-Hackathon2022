0007a98743b2fecd9d05a7bcf4425c8e1af030cd
고동재 <ehdwo98@knu.ac.kr>
2022-09-24 22:48:47 +0900

    feat:투두리스트생성

--- frontend/src/pages/Activity.jsx
+++ frontend/src/pages/Activity.jsx
@@ -3,16 +3,18 @@ import styled from "styled-components";
 import ActivityCard from "../components/ActivityCard";
 import { Button, Navbar, Nav, Container } from 'react-bootstrap';
 import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import { THEME } from "../constants/colors";
+import usePath from "../hooks/usePath";
 
 
 export default function Activity() {
   let navigate = useNavigate();
+  const { getNthPath } = usePath();
 
   return (
     <Layout title="활동 페이지" hasBackButton>
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">활동</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link onClick={()=>{ navigate('/activity')}}>Outside</Nav.Link>
             <Nav.Link onClick={()=>{ navigate('/activity/inside')}}>Inside</Nav.Link>
@@ -26,7 +28,7 @@ export default function Activity() {
               [1,2,3,4,5,6,7].map(()=>{
                 return(
                   <>
-                    <div class="col-sm"><ActivityCard/></div>
+                    <div class="col-sm-4"><ActivityCard/></div>
                   </>
                 )
               })
