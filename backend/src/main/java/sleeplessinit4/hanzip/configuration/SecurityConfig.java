844624832306f968844b5df1aed3f2540e743260
ChangEn Yea <nsce9806q@gmail.com>
2022-09-25 02:25:42 +0900

    Config: Cors & Security 설정

---
+++ backend/src/main/java/sleeplessinit4/hanzip/configuration/SecurityConfig.java
@@ -0,0 +1,31 @@
+package sleeplessinit4.hanzip.configuration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig {
+  private final CorsConfig corsConfig;
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .addFilter(corsConfig.corsFilter())
+        .csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .formLogin().disable()
+        .httpBasic().disable()
+        .authorizeRequests()
+        .anyRequest().permitAll();
+
+    return http.build();
+  }
+}
