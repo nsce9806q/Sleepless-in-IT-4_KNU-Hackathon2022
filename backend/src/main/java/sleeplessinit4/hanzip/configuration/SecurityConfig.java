d226c8a0f2787bbb31e2f7e82359b1973f399811
ChangEn Yea <nsce9806q@gmail.com>
2022-09-25 04:39:18 +0900

    Feature: 상태 검사 프로토콜 (root) 추가

--- backend/src/main/java/sleeplessinit4/hanzip/configuration/SecurityConfig.java
+++ backend/src/main/java/sleeplessinit4/hanzip/configuration/SecurityConfig.java
@@ -1,10 +1,8 @@
 package sleeplessinit4.hanzip.configuration;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
--- backend/src/main/java/sleeplessinit4/hanzip/controller/MainPageController.java
+++ backend/src/main/java/sleeplessinit4/hanzip/controller/MainPageController.java
@@ -1,13 +1,19 @@
 package sleeplessinit4.hanzip.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainPageController {
+
   @GetMapping("/")
-  public ResponseEntity<String> root(){
-    return ResponseEntity.ok().body("hello world");
+  public ResponseEntity<Map<String,Object>> root(){
+    Map<String,Object> response = new HashMap<>();
+    response.put("hello","world");
+
+    return ResponseEntity.ok().body(response);
   }
 }
